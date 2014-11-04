913f29e4c0cfde631d954879c06bd78e9f26087a
Etienne THEODORE <etheodor@e3r2p18.42.fr>
2014-11-04 09:53:36 +0100

    clear

--- includes/libft.h
+++ includes/libft.h
@@ -6,7 +6,7 @@
 /*   By: etheodor <etheodor@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2014/11/03 15:38:20 by etheodor          #+#    #+#             */
-/*   Updated: 2014/11/03 15:38:21 by etheodor         ###   ########.fr       */
+/*   Updated: 2014/11/04 09:53:05 by etheodor         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,11 +19,12 @@
 # include <stdlib.h>
 # include <unistd.h>
 
-int				ft_strlen(char *str);
-void			ft_putchar(char c);
-void			ft_putstr(char *str);
-void			ft_putnbr(int nb);
-void			*ft_memset(void *b, int c, unsigned int len);
-void			ft_swap(int *a, int *b);
-void			ft_putnbr(int nb);
+void	ft_bzero(void *s, size_t n);
+int		ft_strlen(char *str);
+void	ft_putchar(char c);
+void	ft_putstr(char *str);
+void	ft_putnbr(int nb);
+void	*ft_memset(void *b, int c, unsigned int len);
+void	ft_swap(int *a, int *b);
+void	ft_putnbr(int nb);
 #endif
