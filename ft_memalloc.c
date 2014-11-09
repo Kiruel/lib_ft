5bf5b1469daaa499893088d5ff0992f0d90b7b50
Etienne Theodore <etiennetheodore@pc5.home>
2014-11-09 20:02:34 +0100

    add memalloc

--- ft_memalloc.c
+++ ft_memalloc.c
@@ -4,7 +4,13 @@
 void	*ft_memalloc(size_t size)
 {
   char *new;
+  size_t i;
 
   new = (char*)malloc(sizeof(char) * size);
+  while (i < size)
+  {
+  	size[i] = 0;
+  	i++;
+  }
   return (new);
 }
